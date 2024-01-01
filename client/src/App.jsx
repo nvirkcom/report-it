28152baa032eb1d2d4fd9a330ef86cbd4d1a95ba
Navpreet Virk <nvirkcom@gmail.com>
2024-01-01 02:11:41 +0000

    client: import Tailwind into project, install and set up React Router and create /login and 404 route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.scss";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Navigate to="/login" />} path="/" />
+        <Route element={<div>Login</div>} path="/login" />
+        <Route element={<div>404</div>} path="*" />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
